e0fb4006a89f05934368367bb093d53d32bc12a9
azurnaci <azurnaci@cultwinesltd.com>
2023-03-17 17:03:42 +0000

    Basic Todo List fe and be

---
+++ WepApp/Todo/Controllers/TodoController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public TodoController(IMemoryCache memoryCache)
+        {
+            // Inject IMemoryCache interface in the constructor
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public async Task<List<Todos>> GetTodoCacheAsync()
+        {
+            var output = _memoryCache.Get<List<Todos>>("todos");
+            if(output != null) return output;
+
+            output = new List<Todos>()
+            {
+                new Todos() { Id = 1, Todo = "Go to Hell!", Status= "Active" },
+                new Todos() { Id = 2, Todo = "Go to Hell Again!", Status= "Active" },
+                new Todos() { Id = 3, Todo = "Go to Hell and Stay There!", Status= "Active" }
+            };
+
+            await Task.Delay(500);
+
+            _memoryCache.Set("todos", output, TimeSpan.FromMinutes(1));
+
+            return output;
+        }
+    }
+}
---
+++ WepApp/Todo/Models/Todos.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Models
+{
+    public class Todos
+    {
+        public int Id { get; set; }
+        public string Todo { get; set; }
+        public string Status { get; set; }
+    }
+}
